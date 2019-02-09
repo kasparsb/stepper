edda5bf063156acf923a89a6d381a4f0c8d463f1
Kaspars Bulins <kasparsb@hotmail.com>
2019-02-09 21:39:27 +0000

    Rewritten run and runFrom signature, added config override

--- stepper.js
+++ stepper.js
@@ -1,24 +1,34 @@
 //var Bezier = require('./bezier1.js');
+/**
+ * See https://easings.net
+ */
 var Bezier = require('./bezier2.js');
 
-var Stepper = function() {
+var Stepper = function(config) {
     this.defaultBezierCurve = [0,0,1,1];
     this.precision = 10000000;
     this.progress = 0;
     this.current = 0;
     this.requestId = 0;
     this.inProgress = false;
+
+    this.config = config;
+
+    this.setConfig();
 }
 
 Stepper.prototype = {
-    run: function(duration, bezierCurve, stepCb, doneCb, forceStopCb) {
-        this.stepCallback = stepCb;
-        this.doneCallback = doneCb;
-        this.forceStopCallback = forceStopCb;
+    setConfig: function(overrideConfig) {
+        this.duration = this.getConfig('duration', overrideConfig);
+        this.easing = this.getConfig('bezierCurve', overrideConfig);
+        this.stepCallback = this.getConfig('onStep', overrideConfig);
+        this.doneCallback = this.getConfig('onDone', overrideConfig);
+        this.forceStopCallback = this.getConfig('onForceStop', overrideConfig);
+    },
 
-        this.easing = this.getEasing(bezierCurve);
+    run: function(overrideConfig) {
+        this.setConfig(overrideConfig);
 
-        this.duration = isNaN(duration) ? 0 : duration;
         this.current = 0;
 
         this.start();
@@ -28,11 +38,8 @@ Stepper.prototype = {
     /**
      * Run from given progress
      */
-    runFrom: function(progress, duration, bezierCurve, stepCb, doneCb) {
-        this.stepCallback = stepCb;
-        this.doneCallback = doneCb
-
-        this.easing = this.getEasing(bezierCurve);
+    runFrom: function(progress, overrideConfig) {
+        this.setConfig(overrideConfig);
 
         /**
          * Šeit ir svarīgs moments
@@ -40,19 +47,22 @@ Stepper.prototype = {
          * bet easing aprēķinātais progress esošajā progress ir savādāk, jo 
          * tas ir curve un tas nav lineārs
          * Tāpēc šeit atrodam kādam ir jābūt progresam pēc easing
+         *
+         * Update pēc kāda laika lietošanas
+         * Tomēr škiet, ka to nemaz nevajag darīt, jo ja es gribu sākt no
+         * 0.9 progresa un duration ir 1000ms, tad es gribu, lai animācija
+         * ir 100ms gara. Ja pārrēķina progresu, tad animācijas garums ir 
+         * garāks vai īsāks
          */
-        progress = this.findStartProgress(progress, 0.1, 0, 1);
+        //progress = this.findStartProgress(progress, 0.1, 0, 1);
         
-        this.duration = duration;
-
         this.startTime = +new Date();
         // Simulējam startTime, tā lai tas būtu sācies pirms norādītā progress
-        this.startTime -= (duration * progress);
+        this.startTime -= (this.duration * progress);
         // Turpinām no padotā progress
         this.progress = progress;
 
         this.inProgress = true;
-
 
         this.step();
     },
@@ -60,47 +70,47 @@ Stepper.prototype = {
     /**
      * Meklējam kādam ir jābūt progress, lai pēc easing.get tas būt tāds pats kā progress
      */
-    findStartProgress: function(progress, step, from, to, inceptionLevel) {
+    // findStartProgress: function(progress, step, from, to, inceptionLevel) {
 
-        if (typeof inceptionLevel == 'undefined') {
-            inceptionLevel = 0;
-        }
+    //     if (typeof inceptionLevel == 'undefined') {
+    //         inceptionLevel = 0;
+    //     }
         
-        var d = from, prevR = 0, prevD;
+    //     var d = from, prevR = 0, prevD;
 
-        while (d < to) {
-            // Lai ir lielāka precizītāte
-            if (inceptionLevel++ > 40) {
-                return d;
-            }
+    //     while (d < to) {
+    //         // Lai ir lielāka precizitāte
+    //         if (inceptionLevel++ > 100) {
+    //             return d;
+    //         }
 
-            r = this.easing.get(d);
+    //         r = this.easing.get(d);
 
-            if (Math.round(progress*this.precision) == Math.round(r*this.precision)) {
-                return d;
-            }
+    //         if (Math.round(progress*this.precision) == Math.round(r*this.precision)) {
+    //             return d;
+    //         }
 
-            if (this.isBetween(progress, prevR, r)) {
-                return this.findStartProgress(progress, step/10, prevD, d, inceptionLevel);
-            }
+    //         if (this.isBetween(progress, prevR, r)) {
+    //             return this.findStartProgress(progress, step/10, prevD, d, inceptionLevel);
+    //         }
 
-            prevD = d;
-            prevR = r;
-            d += step;
-        }
+    //         prevD = d;
+    //         prevR = r;
+    //         d += step;
+    //     }
 
-        return d;
-    },
+    //     return d;
+    // },
 
     /**
      * Is a between x1 and x2
      */
-    isBetween: function(a, x1, x2) {
-        if (x2 > x1) {
-            return a > x1 && a < x2;
-        }
-        return a > x2 && a < x1;
-    },
+    // isBetween: function(a, x1, x2) {
+    //     if (x2 > x1) {
+    //         return a > x1 && a < x2;
+    //     }
+    //     return a > x2 && a < x1;
+    // },
 
     isRunning: function() {
         return this.inProgress;
@@ -144,13 +154,6 @@ Stepper.prototype = {
     step: function() {
         var mthis = this;
 
-
-        // if (this._prevTime) {
-        //     log('stepper.step', Math.round(window.performance.now() - this._prevTime));
-        // }
-        // this._prevTime = window.performance.now();
-
-
         mthis.trackProgress();
 
         if (this.current < this.startTime + this.duration) {
@@ -185,6 +188,44 @@ Stepper.prototype = {
             bezierCurve = this.defaultBezierCurve;
         }
         return new Bezier(bezierCurve[0], bezierCurve[1], bezierCurve[2], bezierCurve[3]);
+    },
+
+    /**
+     * Atgriežam config vērtību.
+     * Katrai vērtībai pēc tās name tiek veiktas pārbaudes vai papildus apstrāde
+     * @param secondaryConfig object Alternate override config vērtības
+     */
+    getConfig: function(name, secondaryConfig) {
+        var r = this.getConfigValue(name, secondaryConfig);
+        switch (name) {
+            case 'bezierCurve':
+                r = this.getEasing(r);
+                break;
+            case 'duration':
+                r = parseInt(r, 10);
+                r = isNaN(r) ? 200 : r;
+                break;
+            case 'onStep':
+            case 'onDone':
+            case 'onForceStop':
+                r = typeof r == 'function' ? r : function(){}
+        }
+        
+        return r;
+    },
+
+    getConfigValue: function(name, secondaryConfig) {
+        // Pirmo meklējam sekundārajā konfigā
+        if (secondaryConfig && typeof secondaryConfig[name] != 'undefined') {
+            return secondaryConfig[name];
+        }
+
+        // Meklējam bāzes konfigā
+        if (this.config && typeof this.config[name] != 'undefined') {
+            return this.config[name];
+        }
+
+        return undefined;
     }
 }
 
